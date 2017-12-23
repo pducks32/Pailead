d0c0be78ae5beab3263be5271ad1f9b0b1eb1874
Patrick Metcalfe <git@patrickmetcalfe.com>
2017-12-23 15:46:36 +0000

    add documentation

--- Sources/Pailead/VBox.swift
+++ Sources/Pailead/VBox.swift
@@ -12,7 +12,6 @@ public class VBox : Hashable {
         return lhs.hashValue == rhs.hashValue
     }
     
-    
     public enum Axis : CustomStringConvertible {
         public static let all : [Axis] = [.red, .green, .blue]
         case red, green, blue
@@ -30,20 +29,35 @@ public class VBox : Hashable {
         return minPixel.rawValue ^ maxPixel.rawValue
     }
     
+    /// The superposition of **minimum** subvaues in each dimension
     public var minPixel : Pixel
+    /// The superposition of **maximum** subvaues in each dimension
     public var maxPixel : Pixel
+    /// The pixels that lie within the vbox
     public var contents : [Pixel : Int] = [:]
     
+    //// The volume in rgb space that the vbox occupies
     public var volume : Pixel.SubValue {
         return redExtent * greenExtent * blueExtent
     }
     
+    
+    /// Create a new VBox with contents
+    ///
+    /// - Parameters:
+    ///   - min: The minimum possible pixel
+    ///   - max: The maximum possible pixel
+    ///   - contents: The pixels and their frequencies
     public init(min : Pixel, max : Pixel, contents : [Pixel: Int]) {
         self.minPixel = min
         self.maxPixel = max
         self.contents = contents
     }
     
+    
+    /// Create a new VBox and calculate min and max pixels
+    ///
+    /// - Parameter pixels: The pixels and their frequencies
     public init(pixels : [Pixel : Int]) {
         var redMin   : Pixel.SubValue = 256
         var greenMin : Pixel.SubValue = 256
@@ -78,6 +92,10 @@ public class VBox : Hashable {
         self.contents = pixels
     }
     
+    
+    /// Create a new Vbox and calculate the pixel frequencies and min/max pixels
+    ///
+    /// - Parameter pixels: All pixels in the vbox
     public init(pixels : [Pixel]) {
         var redMin   : Pixel.SubValue = 256
         var greenMin : Pixel.SubValue = 256
@@ -115,11 +133,16 @@ public class VBox : Hashable {
         self.maxPixel = Pixel(red: redMax, green: greenMax, blue: blueMax)
         self.contents = contents
     }
-    
+
+    /// If the box has room to actually split.
     public var canSplit : Bool {
-        return redLength >= 1
+        return volume >= 2
     }
     
+    /// Split the vbox into 2 equally populated vboxes
+    ///
+    /// - Todo: Calculate min and max so double loop won't be needed.
+    /// - Returns: The two vboxes
     public func split() -> (VBox, VBox) {
         
         // How the fuck do I do this. A partition?
@@ -138,9 +161,13 @@ public class VBox : Hashable {
         return (VBox(pixels: smaller), VBox(pixels: larger))
     }
     
+    /// Calculate the median point at which point there is an equal
+    /// number of points on both sides of the split.
+    ///
+    /// - Parameter dimension: The dimension to split against
+    /// - Returns: The median subvalue
     public func median(along dimension : Axis) -> Pixel.SubValue {
         var totalSum = 0
-        //var slicesCumSum : [Int] = []
         
         let lengthOfLongest = length(along: dimension)
         
@@ -180,6 +207,7 @@ public class VBox : Hashable {
         fatalError("Really confused right now")
     }
     
+    /// Lowest subvalue in given axis
     public func inital(in axis : Axis) -> Pixel.SubValue {
         switch axis {
         case .red:
@@ -191,6 +219,7 @@ public class VBox : Hashable {
         }
     }
     
+    /// Highest subvalue in given axis
     public func final(in axis : Axis) -> Pixel.SubValue {
         switch axis {
         case .red:
@@ -202,6 +231,7 @@ public class VBox : Hashable {
         }
     }
     
+    /// Subvalue bounds in given axis
     public func extremities(in axis : Axis) -> (lower : Pixel.SubValue, upper : Pixel.SubValue) {
         switch axis {
         case .red:
@@ -213,6 +243,11 @@ public class VBox : Hashable {
         }
     }
     
+    
+    /// The statistical average pixel value
+    ///
+    /// - Complexity: O(n)
+    /// - Returns: the average subvalues in each dimension as a pixel
     public func average() -> Pixel {
         var totalPopulation = 0
         var redSum = 0
@@ -234,6 +269,7 @@ public class VBox : Hashable {
         return Pixel(red: Pixel.SubValue(finalRed), green: Pixel.SubValue(finalGreen), blue: Pixel.SubValue(finalBlue))
     }
     
+    /// The dimension with the greatest extent
     var longestDimension : Axis {
         return [Axis.red, Axis.green, Axis.blue].max { (first, second) -> Bool in
             // is Second larger than First?
@@ -241,6 +277,10 @@ public class VBox : Hashable {
         }!
     }
     
+    /// The length along a given axis
+    ///
+    /// - Parameter axis: the axis to use
+    /// - Returns: Subvalue representing the length along dimension
     public func length(along axis : Axis) -> Pixel.SubValue {
         switch axis {
         case .red:
@@ -252,11 +292,19 @@ public class VBox : Hashable {
         }
     }
     
-    public func midpoint(in dimension : Axis) -> Pixel.SubValue {
-        let (lower, upper) = self.extremities(in: dimension)
+    /// Halfway point along a given axis
+    ///
+    /// - Parameter axis: the axis to use
+    /// - Returns: the subvalue that is halfway between min and max
+    public func midpoint(in axis : Axis) -> Pixel.SubValue {
+        let (lower, upper) = self.extremities(in: axis)
         return lower + (upper - lower) / 2
     }
     
+    /// Check if the pixel is within the vbox's contents
+    ///
+    /// - Parameter pixel: the pixel to check
+    /// - Returns: Whether the contents of the vbox contains the pixel
     public func contains(_ pixel : Pixel) -> Bool {
         if let _ = contents.index(forKey: pixel) {
             return true
@@ -265,61 +313,72 @@ public class VBox : Hashable {
         }
     }
     
+    /// Check if pixel is within bounds of vbox
+    ///
+    /// - Parameter pixel: the pixel to check
+    /// - Returns: whether the pixel lies within the box
     public func covers(_ pixel : Pixel) -> Bool {
         return covers(value: pixel.red, in: .red) &&
             covers(value: pixel.green, in: .green) &&
             covers(value: pixel.blue, in: .blue)
     }
     
+    /// Check if a pixel subvalue is within the bounds of the vbox
+    ///
+    /// - Parameters:
+    ///   - value: the subvalue to be evaluated
+    ///   - axis: along which acess to check for subvalue
+    /// - Returns: whether the subvalue lies within the bounds of the axis
     public func covers(value : Pixel.SubValue, in axis : Axis) -> Bool {
         let extremitiesInAxis = extremities(in: axis)
         return extremitiesInAxis.lower <= value && value <= extremitiesInAxis.upper
     }
     
+    /// Lowest red value in box
     public var initialRed : Pixel.SubValue {
         return minPixel.red
     }
-    
+    /// Largest red value in box
     public var finalRed : Pixel.SubValue {
         return maxPixel.red
     }
-    
+    /// Lowest green value in box
     public var initialGreen : Pixel.SubValue {
         return minPixel.green
     }
-    
+    /// Largest green value in box
     public var finalGreen : Pixel.SubValue {
         return maxPixel.green
     }
-    
+    /// Lowest blue value in box
     public var initialBlue : Pixel.SubValue {
         return minPixel.blue
     }
-    
+    /// Largest blue value in box
     public var finalBlue : Pixel.SubValue {
         return maxPixel.blue
     }
-    
+    /// The distance between min and max red values
     public var redLength : Pixel.SubValue {
         return finalRed - initialRed
     }
-    
+    /// The distance along red this box should take up in the world
     public var redExtent : Pixel.SubValue {
         return redLength + 1
     }
-    
+    /// The distance between min and max green values
     public var greenLength : Pixel.SubValue {
         return finalGreen - initialGreen
     }
-    
+    /// The distance along green this box should take up in the world
     public var greenExtent : Pixel.SubValue {
         return greenLength + 1
     }
-    
+    /// The distance between min and max blue values
     public var blueLength : Pixel.SubValue {
         return finalBlue - initialBlue
     }
-    
+    /// The distance along blue this box should take up in the world
     public var blueExtent : Pixel.SubValue {
         return blueLength + 1
     }
